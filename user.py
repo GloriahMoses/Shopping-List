07259d2751e3f1741c0814ed5e408cf3a3d9eb07
GloriahMoses <mosesgloriah96@gmail.com>
2017-10-16 01:08:40 +0300

    Updated user funcionality file

--- user.py
+++ user.py
@@ -1,4 +1,4 @@
-users = {} #Initializing a global user dict
+users_lists = {} #Initializing a global user dict
 class User(object):
     """
     Class for user functionionality
@@ -13,34 +13,33 @@ class User(object):
     def registration(self, name, email, password, cpassword):
         """registration method"""
         if name != '' and email != ''and password != '' and cpassword != '':
-            if email not in users.keys():
+            if email not in users_lists.keys():
                 if password == cpassword:
-                    users[email] = {'name': name,'pass': password}
-                    print(users)
+                    users_lists[email] = {'name': name,'password': password}
                     return 1 #success,login page
                 else:
-                    return "Password dont match"
+                    return 2 #Password dont match
             else:
-                return "Email already registred"
+                return 3 #Email already registred
         else:
-            return "Fill in all the fields"
+            return 4 #Fill in all the fields
 
     def login(self, email, password):
         """ Login method"""
-        if email in users.keys():
-            if password == users[email][1]:
-                return "Login successfull"
+        if email in users_lists.keys():
+            if password == users_lists[email]['password']:
+                return 5 #Login successfull
             else:
-                return "Wrong password"
+                return 6 #Wrong password
         else:
-            return "Email not registered"
+            return 7 #Invalid Email
 
     def resetpassword(self, name, email, npassword, ncpassword):
         """Reset password method """
         if email != '':
-            if email in users.keys():
+            if email in users_lists.keys():
                 if npassword == ncpassword:
-                    users[email] = [name, npassword]
+                    users_lists[email] = [name, npassword]
                     return "Successfully changed password"
                 else:
                     return "Passwords do not match"
@@ -48,3 +47,10 @@ class User(object):
                 return "Email not registered"
         else:
             return "Email cant be blank"
+
+
+"""gilo = User()
+gilo.registration( (input("name:")),  (input("email:")), (input("passw:")), (input("cpass:")))
+
+gilo.login( (input("email:")), (input("pass:")) )
+"""
