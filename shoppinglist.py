78e8111a06d627f39e037deee01eeb847d87daba
GloriahMoses <mosesgloriah96@gmail.com>
2017-10-19 02:35:27 +0300

    Shopping list functonality

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,6 +1,6 @@
 from user import User
 shoppinglist = {}
-item_list = {}
+
 class Shoppinglist(object):
 	"""
     Class for user functionionality
@@ -23,19 +23,4 @@ class Shoppinglist(object):
 				return "List already exists"
 		else:
 			return "Fill in all the details"
-
-	def owner_items(self, user, list_name):
-        user_items = [item for item in self.item_list if item['owner']
-                      == user and item['list'] == list_name]
-        return user_items
-
-	def add(self, title, name, price, quantity):
-			"""add items method"""
-			if title != '' and name != '' and price != '' and quantity != '':
-				items[title] = {"name": {'price':price, 'quantity': quantity, 'budget':budget, 'owner':owner}}
-				print (items)
-				return 1
-			else:
-				return "Fill in all the details"
-
 	
