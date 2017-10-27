e4270065bb92b884d09bd437e440b5892a24e34b
GloriahMoses <mosesgloriah96@gmail.com>
2017-10-27 11:19:15 +0300

    Add items functionality file

--- items.py
+++ items.py
@@ -1,19 +1,45 @@
 from user import User
 from shoppinglist import Shoppinglist
 
-items_dict = {}
+items_dict = {'title':{'Name': 'Sugar', 'Quantity': '2kg', 'Budget': '200'}}
 class Shoppingitems(object):
+	"""
+    Class for shoppinglist functionionality
+    """
 
 	def __init__(self, title=None, owner=None ):
 		""" Initializing variables"""
 		self.title = title
 		self.owner = owner
 
-	def add(self,title, name, quantity, budget, owner):
+	def add(self,title, item_name, quantity, budget):
 			"""add items method"""
-			if name != '' and quantity !='' and budget !='':
-				if name not in items_dict:
-					items_dict[title] = {'Name': name, 'Quantity': quantity, 'Budget': budget}
-					print (items_dict)
-					return 9
-				print ("anything")
+			print (title)
+			if item_name != '' and quantity !='' and budget !='':
+				for item_names in items_dict:
+					if item_name not in items_dict.keys():
+						items_dict['title'] = {'Name': item_name, 'Quantity': quantity, 'Budget': budget}
+						print (items_dict)
+						return 9
+					else:
+						return 3
+			else:
+				return 4
+
+	def read_list_items(self,title,owner, ):
+		if owner == owner:
+			if title == items_dict[title]:
+				list_items = items_dict
+				return list_items
+
+	"""def edit_item(self, owner, title, name):
+		for name in range(len(self.items_dict)):
+			if self.items_dict[name]['name'] == name:
+				items_dict['title'] = {'Name': Nname, 'Quantity': Nquantity, 'Budget': Nbudget, 'Owner': owner} 
+
+	def delete_item(self, owner, title, name):
+		for item in range(len(self.items_dict)):
+			if self.items_dict[name]['name'] == name:
+				del self.item_list[item]
+	"""
+                
