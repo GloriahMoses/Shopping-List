739216c86c30c131f8b00024c10511e5191af782
GloriahMoses <mosesgloriah96@gmail.com>
2017-11-11 01:10:49 +0300

    Update shoppinglist.py

--- shoppinglist.py
+++ shoppinglist.py
@@ -43,7 +43,7 @@ class Shoppinglist(object):
 		self.item = items_dict[title].keys()
 
 	def view(self, items_dict, lists):
-		#self.items_dict = items_dict[title][item_name]
-		self.items_dict = items_dict
+		self.items_dict = items_dict[title][item_name]
+		#self.items_dict = items_dict
 		self.lists = shoppinglists[title]
 
