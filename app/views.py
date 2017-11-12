ec256504b3b3f1a03a2ca97be92f61f981481a2d
GloriahMoses <mosesgloriah96@gmail.com>
2017-11-12 17:44:54 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -54,7 +54,7 @@ def login():
     
         if details == 5: #Login successfull
             session['email'] = request.form['email']
-            return redirect(url_for('view'))
+            return redirect(url_for('view', items_dict = shoppinglist.items_dict, lists = shoppinglist.shoppinglists))
             
         elif details == 6:#Wrong password
           msg = "Wrong email/password, try again"
@@ -75,7 +75,7 @@ def create():
         me_list = userlist.create(title,description,owner)
 
         if me_list == 8:
-            return render_template("view-shopping-list.html")
+            return render_template("view-shopping-list.html", items_dict = shoppinglist.items_dict, lists = shoppinglist.shoppinglists)
 
         if me_list == 10:
             msg = "List already exists"
@@ -111,13 +111,11 @@ def delete_item(itemdel=None):
         for item in shoppinglist.items_dict[title].keys():
             if item == itemdel:
                 shoppinglist.items_dict[title].pop(item)
-                return render_template("view-shopping-list.html")
+                return render_template("view-shopping-list.html", items_dict = shoppinglist.items_dict, lists = shoppinglist.shoppinglists)
 
 @app.route('/view')
 def view():
-    items_dict = shoppinglist.items_dict
-    lists = shoppinglist.shoppinglists
-    return render_template("view-shopping-list.html")
+    return render_template("view-shopping-list.html", items_dict = shoppinglist.items_dict, lists = shoppinglist.shoppinglists)
 
 @app.route('/logout')
 def logout():
