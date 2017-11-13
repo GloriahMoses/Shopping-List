5e113e898722a3d7f8b473564bcb100fb309dcba
GloriahMoses <mosesgloriah96@gmail.com>
2017-11-13 21:49:17 +0300

    Global dictionaries

--- shoppinglist.py
+++ shoppinglist.py
@@ -10,14 +10,14 @@ class Shoppinglist(object):
 		self.title = title
 		self.description = description
 		self.owner = owner
-		shoppinglists = {}
-		items_dict = {}
+		self.shoppinglists = {}
+		self.items_dict = {}
 		
 	def create(self, title, description, owner):
 		"""create list method"""
 		if description != ''and title != '':
-			if title not in shoppinglists.keys():
-				shoppinglists[title] = {'owner':owner, 'Description':description}
+			if title not in self.shoppinglists.keys():
+				self.shoppinglists[title] = {'owner':owner, 'Description':description}
 				return 8 #"Successfully created"
 			else:
 				return 10 #"List already exists"
@@ -27,21 +27,18 @@ class Shoppinglist(object):
 	def add(self,title, item_name, quantity, budget):
 			"""add items method and edit"""
 			if item_name != '' and quantity !='' and budget !='':
-				if title not in items_dict.keys():
-					items_dict[title] = {item_name: [quantity, budget]}
-					print(items_dict)
+				if title not in self.items_dict.keys():
+					self.items_dict[title] = {item_name: [quantity, budget]}
 					return 9
 				else:
-					items_dict[title][item_name] = [quantity, budget]
+					self.items_dict[title][item_name] = [quantity, budget]
 					print(items_dict)
 					return 9
 			else:
 				return 4
 	def view(self):
-		for title in items_dict.keys():
-			title = lists 
-		for item in items_dict[title].keys():
-			items = items_dict
+		self.items_dict = items_dict
+		self.shoppinglists = lists
 			
 	def delete_item(self, item):
 		for title in items_dict.keys():
