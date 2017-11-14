ca3d077f644d42bbbd31defbf35c1013b47a3870
GloriahMoses <mosesgloriah96@gmail.com>
2017-11-14 14:42:42 +0300

    Update shoppinglist.py

--- shoppinglist.py
+++ shoppinglist.py
@@ -33,7 +33,7 @@ class Shoppinglist(object):
 				if title not in shoppinglists.keys():
 					return "Error"
 				else:
-					items_dict.update({title:{item_name: [quantity, budget]}})
+					items_dict.update({title:{item_name[quantity, budget]}})
 					print(items_dict)
 					print(shoppinglists)
 					return 9
