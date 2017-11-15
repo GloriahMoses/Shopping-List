ece1eff734247a2c08dc488894d96f459dfa59e9
GloriahMoses <mosesgloriah96@gmail.com>
2017-11-15 15:43:14 +0300

    Update shoppinglist.py

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,11 +1,8 @@
 from user import User
-import collections
-
 shoppinglists = {}
-items_dict = {}
-	
-class Shoppinglist(object):
 
+class Shoppinglist(object):
+	
 	"""
     Class for shoppinglist functionionality
     """
@@ -14,13 +11,13 @@ class Shoppinglist(object):
 		self.title = title
 		self.description = description
 		self.owner = owner
+		self.items_dict = {}
 		
 	def create(self, title, description, owner):
 		"""create list method"""
 		if description != ''and title != '':
 			if title not in shoppinglists.keys():
 				shoppinglists[title] = {'owner':owner, 'Description':description}
-				print(shoppinglists)
 				return 8 #"Successfully created"
 			else:
 				return 10 #"List already exists"
@@ -30,15 +27,15 @@ class Shoppinglist(object):
 	def add(self,title, item_name, quantity, budget):
 			"""add items method and edit"""
 			if item_name != '' and quantity !='' and budget !='':
-				if title not in shoppinglists.keys():
-					return "Error"
+				if title not in self.items_dict.keys():
+					self.items_dict[title] = {item_name: [quantity, budget]}
+					return 9
 				else:
-					items_dict.update({title:{item_name: [quantity, budget]}})
-					print(items_dict)
-					print(shoppinglists)
+					self.items_dict[title][item_name] = [quantity, budget]
 					return 9
 			else:
 				return 4
+
 	def view(self):
 		print(items_dict)
 		print(shoppinglists)
